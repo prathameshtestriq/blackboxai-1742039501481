a7ba359c90eba09de26b08111919041a5f8df98f
Prathamesh M <67052425+prathameshtestriq@users.noreply.github.com>
2025-03-15 17:22:05 +0530

    Add frontend/babel.config.js 1742039524767

---
+++ frontend/babel.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@navigation': './src/navigation',
+          '@store': './src/store',
+          '@services': './src/services',
+          '@utils': './src/utils',
+          '@constants': './src/constants',
+          '@theme': './src/theme',
+          '@assets': './src/assets',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin', // Required for react-native-reanimated
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+  ],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'], // Remove console.log in production
+    },
+  },
+};
